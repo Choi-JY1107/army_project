9d77de7fcf52e3e285d5f9ea3df1785c11873ab0
홍혁준 <gurwns9325@naver.com>
2022-09-01 13:52:11 +0900

    backend init

---
+++ backend/outManegement/src/main/java/com/example/outManegement/OutManegementApplication.java
@@ -0,0 +1,13 @@
+package com.example.outManegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutManegementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutManegementApplication.class, args);
+	}
+
+}
---
+++ backend/outManegement/src/test/java/com/example/outManegement/OutManegementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outManegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutManegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
