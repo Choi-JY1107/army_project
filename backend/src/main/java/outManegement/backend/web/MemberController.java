d9dbc322411f2603fec712511cc440c52e9291a7
홍혁준 <gurwns9325@naver.com>
2022-09-07 15:40:55 +0900

    init Controller

---
+++ backend/src/main/java/outManegement/backend/web/MemberController.java
@@ -0,0 +1,10 @@
+package outManegement.backend.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/members")
+public class MemberController {
+    
+}
